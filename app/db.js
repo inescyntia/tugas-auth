de9237660b57581fb2d07c64e061294de083c737
inescyntia <inescyntiaaa@gmail.com>
2022-02-22 05:35:36 +0700

    migrateing database

--- app/db.js
+++ app/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: "postgres://ivurnzpbhiepma:70db5dc37da06841b0b0ae43473b08672d0762980d6ec8b2a53c84e2307c1bb7@ec2-54-174-43-13.compute-1.amazonaws.com:5432/d1shrjv8il5lij",
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 })
 
