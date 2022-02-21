d5050cbd76115c40fea847f4126c841fea21c501
inescyntia <inescyntiaaa@gmail.com>
2022-02-22 04:51:06 +0700

    connecting database

--- app/db.js
+++ app/db.js
@@ -1,17 +1,10 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  // isi dengan password, username, dan detail database kalian sendiri
-  host: 'localhost',
-  user: 'postgres',
-  password: 'Watersheep123?',
-  database: 'binar2',
-  // ignore below configuration for now, 
-  // this are pooling connection settings
-  max: 20, // connection pool ignore for now
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
+
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
